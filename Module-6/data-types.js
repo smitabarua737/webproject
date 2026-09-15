cd1cc26cc9f5d948740c32cd33f821a924619ee5
smita <smibarua737@gmail.com>
2026-09-15 10:32:30 +0600

    updated JavaScript data types and symbols

--- Module-6/data-types.js
+++ Module-6/data-types.js
@@ -1,9 +1,9 @@
-let studentName = "Nobita"; // String
-let age = 26; // Number
-let passed = true; // Boolean
+let studentName = "dekisuki"; // String
+let age = 24; // Number
+let passed = false; // Boolean
 let result; // Undefined
 let scholarship = null; // Null
-let largeNumber = 123456789n; // BigInt
+let largeNumber = 987654321n; // BigInt
 
 console.log(typeof studentName);
 console.log(typeof age);
@@ -13,8 +13,8 @@ console.log(typeof scholarship);
 console.log(typeof largeNumber);
 
 
-const sym1 = Symbol("id");
-const sym2 = Symbol("id");
+const sym1 = Symbol("studentid");
+const sym2 = Symbol("studentid");
 
 console.log(sym1 === sym2); // false
-console.log(sym1.description); // "id"
+console.log(sym1.description); // "studentid"
