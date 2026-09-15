db34ba64df3a872f87dd3b6c64fadd3d90d45bb2
smita <smibarua737@gmail.com>
2026-09-15 11:00:24 +0600

    updated JavaScript template literals

--- Module-6/literals.js
+++ Module-6/literals.js
@@ -1,8 +1,8 @@
 // Template literals in JavaScript
-// `(backticks) are used to create template literals, which allow for string interpolation and multi-line strings.
-
-const name = "Bill Gates";
-const age = 26;
+// `(backticks) are used to create template literals.
+// They allow string interpolation and multi-line strings.
+const name = "Smita";
+const age = 21;
 
 const message = `My name is ${name} and I am ${age} years old.`;
 
@@ -10,17 +10,17 @@ console.log(message);
 
 // Expression inside template literal
 
-const a = 10;
-const b = 20;
+const a = 15;
+const b = 25;
 
 console.log(`The sum is ${a + b}`);
 
 // Multi-line string
 
 const message2 = `
-Name: Bill Gates
-Age: 26
-Department: CSE
+Name: smita
+Age: 21
+Department:  Software Engineering
 `;
 
 console.log(message2);
